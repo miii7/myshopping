652dc0e293d9346d561a2421eb6d94b5a061fd65
yuuu <miii888prog@gmail.com>
2020-11-05 07:35:09 +0000

    user registration

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -5,6 +5,8 @@
         <div class="text-center">
             <h1>My Shopping</h1>
             <p>Really Want? or Want?</p>
+            {{-- ユーザ登録ページへのリンク --}}
+           {!! link_to_route('signup.get', 'Let's start', [], ['class' => 'btn btn-lg btn-primary']) !!}
         </div>
     </div>
 @endsection
